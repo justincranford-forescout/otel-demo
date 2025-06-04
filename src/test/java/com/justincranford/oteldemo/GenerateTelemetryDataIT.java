80e216fc9239905a4173d49172d843655f9101de
Justin Cranford <justin.cranford@forescout.com>
2025-06-04 09:58:50 -0400

    Cleaned up GenerateTelemetryDataIT

--- src/test/java/com/justincranford/oteldemo/GenerateTelemetryDataIT.java
+++ src/test/java/com/justincranford/oteldemo/GenerateTelemetryDataIT.java
@@ -32,30 +32,35 @@ class GenerateTelemetryDataIT extends AbstractIT {
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForLogs() {
+    void logs() {
+        log.info("Starting logs thread...");
         doLoopInThread(currentIteration -> log.info("Log message {}", currentIteration), 1000, 3000, "logging");
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForCounterMetric() {
+    void counterMetric() {
+        log.info("Starting counterMetric thread...");
         final Counter counter = Counter.builder("example.counter").description("counter example").baseUnit("tasks").tags("a", "1", "b", "2").register(super.meterRegistry());
         doLoopInThread(currentIteration ->  counter.increment(), 500, 1000, "increment counter");
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForHistogramMetric() {
+    void histogramMetric() {
+        log.info("Starting histogramMetric thread...");
         final DistributionSummary histogram = DistributionSummary.builder("example.histogram").description("histogram example").baseUnit("ms").tags("b", "2", "c", "3").register(super.meterRegistry());
         doLoopInThread(currentIteration ->  histogram.record(SECURE_RANDOM.nextDouble(1, 2000)), 500, 1000, "record histogram");
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForGaugeMetric() {
+    void gaugeMetric() {
+        log.info("Starting gaugeMetric thread...");
         final Gauge gauge = Gauge.builder("example.gauge", this, obj -> SECURE_RANDOM.nextInt(0, 100)).description("gauge example").baseUnit("celsius").register(super.meterRegistry());
         doLoopInThread(currentIteration ->  gauge.measure(), 500, 1000, "measure gauge");
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForActuatorTraces() {
+    void actuatorTraces() {
+        log.info("Starting actuatorTraces threads...");
         final RestTemplate restTemplate = new RestTemplateBuilder().build();
         for (final String url : ACTUATOR_ENDPOINTS.stream().map(path -> super.baseUrl() + path).toList()) {
             doLoopInThread(currentIteration -> restTemplate.getForEntity(url, String.class), 500 * ACTUATOR_ENDPOINTS.size(), 1000 * ACTUATOR_ENDPOINTS.size(), "get " + url);
@@ -63,7 +68,8 @@ class GenerateTelemetryDataIT extends AbstractIT {
     }
 
     @Test
-    void generateOpenTelemetryOTLPTrafficForCustomControllerTrace() {
+    void controllerTrace() {
+        log.info("Starting controllerTrace thread...");
         final String url = super.baseUrl() + "/hello";
         final RestTemplate restTemplate = new RestTemplateBuilder().build();
         doLoopInThread(currentIteration -> restTemplate.getForEntity(url, String.class), 500, 1000, "get " + url);
@@ -71,8 +77,9 @@ class GenerateTelemetryDataIT extends AbstractIT {
 
     @Order(Integer.MAX_VALUE)
     @Test
-    void generateOpenTelemetryOTLPTrafficForCustomControllerMetrics() {
-        if (livedemo) {
+    void livedemo() {
+        log.info("livedemo: {}", livedemo);
+        if (this.livedemo) {
             log.info("Waiting for interrupt before exiting...");
             while (true) {
                 try {
