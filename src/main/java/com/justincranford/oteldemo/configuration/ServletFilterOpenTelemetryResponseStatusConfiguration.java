13efbe3da526652dc302af66faec665fc30a5b8d
Justin Cranford <justin.cranford@forescout.com>
2025-06-05 14:37:35 -0400

    Added OtelStatusFilter to set status in Grafana Tempo exemplar popups to expected `ok` or `error`

---
+++ src/main/java/com/justincranford/oteldemo/configuration/ServletFilterOpenTelemetryResponseStatusConfiguration.java
@@ -0,0 +1,38 @@
+package com.justincranford.oteldemo.configuration;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.servlet.Filter;
+
+import java.io.IOException;
+
+@Configuration
+public class ServletFilterOpenTelemetryResponseStatusConfiguration {
+    @Bean
+    public Filter otelStatusFilter() {
+        return new OtelStatusFilter();
+    }
+
+    public static class OtelStatusFilter implements Filter {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            try {
+                chain.doFilter(request, response);
+            } finally {
+                int status = ((HttpServletResponse) response).getStatus();
+                Span span = Span.current();
+                if (status >= 200 && status < 300) {
+                    span.setStatus(StatusCode.OK);
+                } else {
+                    span.setStatus(StatusCode.ERROR);
+                }
+            }
+        }
+    }}
