b8e8bbfb17cd1cced140c22eec0ef8a966d1e38a
Justin Cranford <justin.cranford@forescout.com>
2025-07-24 10:36:08 -0400

    Added Asset example entity

---
+++ src/main/java/com/justincranford/oteldemo/entity/Asset.java
@@ -0,0 +1,29 @@
+package com.justincranford.oteldemo.entity;
+
+import com.justincranford.oteldemo.entity.base.AbstractEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name="assets")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Accessors(fluent=true,chain= true)
+@ToString(callSuper=true)
+@EqualsAndHashCode(callSuper=true)
+@Builder
+@AllArgsConstructor
+public class Asset extends AbstractEntity {
+    @Column(nullable=false,length=100,unique=true)
+    private String name;
+}
