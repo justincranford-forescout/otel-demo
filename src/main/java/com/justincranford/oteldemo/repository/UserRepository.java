ea2f75db17a8f651d0c2917e3f31e49c6d421e26
Justin Cranford <justin.cranford@forescout.com>
2025-07-24 10:35:59 -0400

    Added `@Query` example in UserRepository

--- src/main/java/com/justincranford/oteldemo/repository/UserRepository.java
+++ src/main/java/com/justincranford/oteldemo/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.justincranford.oteldemo.repository;
 
 import com.justincranford.oteldemo.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByName(String caseSensitiveName);
     List<User> findByNameContaining(String caseSensitiveNameSubstring);
 
+    @Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:caseInsensitiveName)")
     List<User> findByNameIgnoreCase(String caseInsensitiveName);
     List<User> findByNameIgnoreCaseContaining(String caseInsensitiveNameSubstring);
 }
